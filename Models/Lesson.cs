59ffc82e13afc3dc2ad7cbdfcfb4564cd0d39c16
Tanmay140503 <ptn8498@gmail.com>
2026-03-21 13:40:33 +0530

    Database Migration

---
+++ Models/Lesson.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learnova.Models.Entities
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public string LessonType { get; set; } // Video, Document, Image, Quiz
+
+        // Type-specific fields
+        public string? VideoUrl { get; set; }
+        public int? DurationMinutes { get; set; }
+        public string? DocumentUrl { get; set; }
+        public string? ImageUrl { get; set; }
+        public bool AllowDownload { get; set; } = false;
+
+        // Order
+        public int OrderIndex { get; set; }
+
+        // Responsible
+        public string? ResponsibleUserId { get; set; }
+        public ApplicationUser? ResponsibleUser { get; set; }
+
+        // Foreign Key
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation Properties
+        public ICollection<LessonAttachment> Attachments { get; set; }
+        public ICollection<LessonProgress> LessonProgresses { get; set; }
+    }
+}
---
+++ Models/QuizQuestion.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace Learnova.Models.Entities
+{
+    public class QuizQuestion
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; }
+        public int OrderIndex { get; set; }
+
+        // Foreign Key
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+
+        // Navigation Properties
+        public ICollection<QuizOption> Options { get; set; }
+    }
+}
