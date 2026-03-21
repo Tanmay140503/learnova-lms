5d1565e71ca0401b51af7696e627e3ede8bb6188
Tanmay140503 <ptn8498@gmail.com>
2026-03-22 01:34:17 +0530

    Delete Services/Interfaces directory

--- Services/Interfaces/IFileService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Learnova.Services.Interfaces
-{
-    public interface IFileService
-    {
-        Task<string> UploadFileAsync(IFormFile file, string folder);
-        Task<bool> DeleteFileAsync(string filePath);
-        string GetFileUrl(string fileName, string folder);
-    }
-}
