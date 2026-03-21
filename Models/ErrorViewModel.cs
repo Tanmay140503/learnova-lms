c9172e0c84d884d493821bdb417ea76a96fc63f9
CodeZen 💻🚀 <jeet.study002@gmail.com>
2026-03-21 17:15:04 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Learnova.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
